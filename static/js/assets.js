bc0f5a536cb8124ecd91657c745fbdf47c68eb14
Swetha1707 <swetha200617@gmail.com>
2026-09-13 00:10:29 +0530

    Included others for department

--- static/js/assets.js
+++ static/js/assets.js
@@ -100,6 +100,21 @@ function handleCategoryChange() {
     input.value = '';
   }
 }
+function handleDepartmentChange() {
+  const department = document.getElementById('af_department_id').value;
+  const group = document.getElementById('newDepartmentGroup');
+  const input = document.getElementById('af_new_department_name');
+
+  if (department === 'OTHERS') {
+    group.style.display = 'block';
+    input.required = true;
+    input.focus();
+  } else {
+    group.style.display = 'none';
+    input.required = false;
+    input.value = '';
+  }
+}
 function openAssetForm(asset = null) {
   document.getElementById('assetForm').reset();
   document.getElementById('newCategoryGroup').style.display = 'none';
@@ -154,7 +169,15 @@ new_category_name:
   document.getElementById('af_category_id').value === 'OTHERS'
     ? document.getElementById('af_new_category_name').value.trim()
     : '',
-        department_id: document.getElementById('af_department_id').value,
+        department_id:
+  document.getElementById('af_department_id').value === 'OTHERS'
+    ? ''
+    : document.getElementById('af_department_id').value,
+
+new_department_name:
+  document.getElementById('af_department_id').value === 'OTHERS'
+    ? document.getElementById('af_new_department_name').value.trim()
+    : '',
     brand: document.getElementById('af_brand').value,
     model: document.getElementById('af_model').value,
     serial_number: document.getElementById('af_serial_number').value,
